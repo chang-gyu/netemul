2dc13d8b54e50c1989af9975d61509ab6ec5fef5
changgyu <changgyu@gurum.cc>
2017-03-27 16:53:10 +0900

    Modifying premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,23 @@
 workspace 'NetEmul'
-		configurations { 'debug', 'release' }
+	configurations 	{ 'debug', 'release' }
+
+    includedirs     { 'sdk', 'include', 'librpc' }
+	defines 		{ '__LINUX' }
+	buildoptions    { '-fms-extensions -std=gnu99' }
+
+project "rpc_netemul"
+    kind 'StaticLib'
+    targetdir "librpc"
+
+    files           { 'librpc/**.c' }
+    defines         { 'LINUX' }
 
 project 'netemul'
 	kind 'ConsoleApp'
     targetdir "."
 
 	files 			{ 'src/**.c' }
-	includedirs 	{ 'sdk', 'include', 'librpc' }
 	buildoptions 	{ '-fms-extensions -std=gnu99' }
-	defines			{ '__LINUX' }
+	defines			{ 'NET_CONTROL'}
 	links 			{ 'umpn', 'rpc_netemul' }
 	libdirs			{ '.', 'librpc' }
